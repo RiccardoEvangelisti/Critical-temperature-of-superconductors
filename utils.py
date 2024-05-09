be821391a5b98640e6beb91736126e990c02f473
RiccardoEvangelisti <evangericcardo00@gmail.com>
2024-05-10 01:47:25 +0200

    update 2 files, delete 1 file and create 2 files

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+import numpy as np
+import pandas as pd
 from sklearn.pipeline import Pipeline
 import itertools
 
@@ -58,7 +61,7 @@ class Pipe:
             parameters.update(**step.parameters)
         self.tag = name_rebuild
         self.pipeline = Pipeline(constructors)
-        keys, values = zip(*parameters.items())
+        keys, values = zip(*parameters.items()) if parameters != {} else ([], [])
         self.parameter_grid = [{k: [v] for k, v in zip(keys, v)} for v in itertools.product(*values)]
 
 
@@ -82,3 +85,31 @@ class MultiplePipes:
         for pipe in pipes:
             for parameters in pipe.parameter_grid:
                 self.combinations.append((pipe.pipeline, parameters, pipe.tag))
+
+
+def combination_already_tested(file_name, parameters, tag):
+    """
+    Check if the combination of given parameters is already tested
+    """
+    if os.path.isfile(file_name):
+        outputs = pd.read_csv(file_name)
+        # Re-create the parameters dictionary to be comparable with the outputs dataset
+        parameters = pd.DataFrame({**parameters, "tag": [tag]}).fillna("NaN")
+        # if all parameters are already present in the output:
+        if all(x in outputs.columns for x in parameters.keys()):
+            # take only the same columns
+            outputs = outputs.loc[:, parameters.keys()].replace(np.nan, "NaN")
+            # find if actual parameters are already present in the output dataset
+            if (outputs == parameters.loc[0]).all(axis=1).any():
+                return True
+    return False
+
+
+def print_results(file_name, head):
+    df = pd.read_csv(file_name)
+    return (
+        df[["tag", "R2", "MSE"] + [col for col in df.columns if col not in ["tag", "R2", "MSE"]]]
+        .sort_values(by="R2", ascending=False)
+        .head(head)
+        .style.format(precision=4)
+    )
